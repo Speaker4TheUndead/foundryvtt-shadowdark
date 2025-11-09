a021f1702c6e94a744037939e3657eea7eeeac5b
Speaker4TheUndead <jchance.is2cool@gmail.com>
2025-11-08 21:17:55 -0600

    Removed Chalk as I was getting vulnerability warnings

--- utils/lib/logger.mjs
+++ utils/lib/logger.mjs
@@ -1,16 +1,19 @@
-import chalk from "chalk";
+//import chalk from "chalk";
+import pc from "picocolors";
 
 export default class Logger {
 
 	static verbose = true;
 
 	static error(message) {
-		console.log(chalk.bold.red(message));
+		//console.log(chalk.bold.red(message));
+		console.log(pc.red(pc.bold(message)));
 	}
 
 	static info(message) {
 		if (!this.verbose) return;
-		console.log(chalk.cyan(message));
+		//console.log(chalk.cyan(message));
+		console.log(pc.cyan(message));
 	}
 
 	static log(message) {
@@ -19,7 +22,8 @@ export default class Logger {
 	}
 
 	static warn(message) {
-		console.log(chalk.hex("#ffa500").bold(message));
+		//console.log(chalk.hex("#ffa500").bold(message));
+		console.log(pc.yellow(pc.bold(message)));
 	}
 
 }
